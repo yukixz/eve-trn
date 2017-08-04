0ba0ea2cd74727ed6a0a6c366494fd366e754a76
Dazzy Ding <h@dazzyd.org>
2017-08-04 11:28:05 +0800

    Add Filter: Common Only

--- src/App.js
+++ src/App.js
@@ -1,58 +1,96 @@
 import React, { Component } from 'react'
-import { Header, Input, Table } from 'semantic-ui-react'
+import { Checkbox, Header, Input, Table } from 'semantic-ui-react'
 import trn from './trn.txt'
 import './App.css'
 const DISPLAY_MAX_INDEX = 20
 
 class App extends Component {
   state = {
-    rows: [],
-    keyword: '',
+    rows   : [],
+    filters: [],
   }
-
-  init = async () => {
-    const resp = await fetch(trn)
-    const data = await resp.text()
-    const rows = data.split('\n').map((r, i) => {
-      const p = r.split('\t')
-      return {
-        id: i,
-        str: r.toLowerCase(),
-        en: p[0],
-        zh: p[1],
-      }
-    })
-    this.setState({rows})
-  }
-
-  handleKeyword = (evt, data) => {
-    this.setState({
-      keyword: data.value || '',
-    })
-  }
+  cacheRows     = []
+  cacheFilters  = []
+  defaultCommon = true
+  defaultSearch = ''
 
   componentDidMount() {
     this.init()
   }
 
+  init = async () => {
+    // Fetch translations
+    const resp = await fetch(trn)
+    const data = await resp.text()
+    const rows = data.split('\n').map((r, id) => {
+      const [en, zh, common] = r.split('\t')
+      return {
+        id, en, zh, common,
+        searchString: `${en}\t${zh}`.toLowerCase(),
+      }
+    })
+    // Init filters
+    const filters = [
+      this.createFilterCommon(this.defaultCommon),
+      this.createFilterSearch(this.defaultSearch),
+    ]
+    // Refresh
+    this.setState({rows, filters})
+  }
+
+  updateFilter = (i, createFilter) => (evt, data) => {
+    const value =
+      data.type === "checkbox" ? data.checked :
+      data.type === "text"     ? data.value   :
+      undefined
+    const filters = this.state.filters.slice()
+    filters[i] = createFilter(value)
+    this.setState({filters})
+  }
+
+  createFilterCommon = (isTrue) => (rows) => {
+    return isTrue === false ? rows :
+      rows.filter(r => r.common === '1')
+  }
+
+  createFilterSearch = (keyword) => (rows) => {
+    return rows.filter(r => r.searchString.includes(keyword))
+  }
+
   render() {
-    const allRows = this.state.rows
-    const keyword = this.state.keyword.toLowerCase()
-    const mthRows = allRows.filter(r => r.str.includes(keyword))
-    const dspRows = mthRows.slice(0, mthRows.length < DISPLAY_MAX_INDEX ? mthRows.length : DISPLAY_MAX_INDEX)
-    const hideNum = mthRows.length - dspRows.length
+    const { filters } = this.state
+    const { cacheRows, cacheFilters } = this
+
+    let rows = this.state.rows
+    let firstMiss = filters.findIndex((f, i) => f !== cacheFilters[i])
+    if (firstMiss > 0)
+      rows = cacheRows[firstMiss - 1]
+    else
+      firstMiss = 0
+    for (let i = firstMiss; i < filters.length; i++) {
+      const filter = filters[i]
+      rows = filter(rows)
+      cacheRows[i] = rows
+      cacheFilters[i] = filter
+    }
+
+    const dspRows = rows.slice(0, rows.length < DISPLAY_MAX_INDEX ? rows.length : DISPLAY_MAX_INDEX)
+    const hideNum = rows.length - dspRows.length
 
     return (
       <div id='wrapper'>
-        <Table id='list' compact striped selectable>
+        <Table id='main' compact striped selectable>
           <Table.Header>
             <Table.Row>
               <Table.HeaderCell>
                 <Header as='h1' content={document.title} />
               </Table.HeaderCell>
               <Table.HeaderCell textAlign='right'>
-                <Input placeholder='Search' onChange={this.handleKeyword}
-                       icon='search' autoFocus />
+                <Checkbox label={'Common Only'}
+                          defaultChecked={this.defaultCommon}
+                          onChange={this.updateFilter(0, this.createFilterCommon)} />
+                <Input placeholder='Search' icon='search' autoFocus
+                       onChange={this.updateFilter(1, this.createFilterSearch)} />
               </Table.HeaderCell>
             </Table.Row>
           </Table.Header>
@@ -73,7 +111,7 @@ class App extends Component {
         </Table>
 
         <footer>
-          <p>&copy; Dazzy Ding | <a href="https://github.com/yukixz/eve-trn">Github</a></p>
+          <p>&copy; <a href="https://dazzyd.org">Dazzy Ding</a> | <a href="https://github.com/yukixz/eve-trn">Github</a></p>
         </footer>
       </div>
     )
